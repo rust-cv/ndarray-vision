eba3eff041be6d88f4605261138893d1a3d90c33
xd009642 <danielmckenna93@gmail.com>
2019-02-13 23:21:06 +0000

    Working on image convolution.
    Nearly got it working (but not quite).

--- src/core/formats.rs
+++ src/core/formats.rs
@@ -1,15 +1,30 @@
 
-
+/// Enum containing supported pixel formats for images. Storage type is
+/// determined by the Image container
+#[derive(Copy, Clone, Eq, PartialEq, Ord, PartialOrd, Hash, Debug)]
 pub enum PixelFormat {
+    /// Single channel intensity image
     Gray,
+    /// Red Green Blue image
     RGB,
+    /// RGB image with an added alpha channel
     RGBA,
+    /// Hue Saturation Value image
     HSV,
+    /// Hue Saturation Intensity image
     HSI,
+    /// Hue Saturation Lightness image
     HSL,
+    /// Y Chroma-red Chroma-blue image
     YCrCb,
+    /// CIE 1931 X Y Z colour space - older standard meant to represent colours
+    /// visible to the human eye
     CIEXYZ,
+    /// CIE 1976 L*a*b* colour space - recommended for characterisation of
+    /// coloured surfaces and dyes
     CIELAB,
+    /// CIE 1976 L* U* V* colour space - recommended for characterisation of 
+    /// colour displays
     CIELUV,
     /// Unspecified layout with the given number of channels
     Other(usize),
@@ -17,7 +32,7 @@ pub enum PixelFormat {
 
 
 impl PixelFormat {
-
+    /// Returns the number of channels used to represent the colour
     pub fn channels(&self) -> usize {
         use PixelFormat::*;
         match self {
--- src/core/image.rs
+++ src/core/image.rs
@@ -1,10 +1,12 @@
 use crate::core::formats::PixelFormat;
-use ndarray::{ArrayView, ArrayViewMut, Array3, Ix1, s};
+use ndarray::{ArrayView, ArrayView3, ArrayViewMut, Array3, Axis, Ix1, s, Zip};
 use num_traits::{Zero, One};
+
 
 pub struct Image<T> {
     /// Images are always going to be 3D to handle rows, columns and colour 
     /// channels
+    ///
     /// 
     /// This should allow for max compatibility with maths ops in ndarray
     pub data: Array3<T>,
@@ -12,7 +14,7 @@ pub struct Image<T> {
     format: PixelFormat,
 }
 
-impl<T> Image<T> where T: One, T: Zero, T: Clone {
+impl<T> Image<T> where T: One, T: Zero, T: Clone, T: Default {
     pub fn new(rows: usize, columns: usize, format: PixelFormat) -> Self {
         Image {
             data: Array3::<T>::zeros((rows, columns, format.channels())),
@@ -27,4 +29,32 @@ impl<T> Image<T> where T: One, T: Zero, T: Clone {
     pub fn pixel_mut(&mut self, row: usize, col: usize) -> ArrayViewMut<T, Ix1> {
         self.data.slice_mut(s![row, col, ..])
     }
+
+    pub fn conv(&self, kernel: ArrayView3<T>) -> Image<T> {
+        Image {
+            data: conv(self.data.view(), kernel),
+            format: self.format
+        }
+    }
+
+    pub fn conv_inplace(&mut self, kernel: ArrayView3<T>) {
+        self.data = conv(self.data.view(), kernel);
+    }
+}
+
+
+pub fn conv<T>(image: ArrayView3<T>, kernel: ArrayView3<T>) -> Array3<T> where T: Default, T: Clone {
+    let mut result = Array3::<T>::default(image.dim());
+    let k_s = kernel.shape();
+    let row_offset = k_s[0]/2;
+    let col_offset = k_s[1]/2;
+
+    Zip::indexed(image.windows(kernel.dim()))
+        .apply(|(i, j, _), window| {
+            let mult = window * kernel;
+            let sums = mult.sum_axis(Axis(2));
+            result.slice_mut(s![i+row_offset, j+col_offset, ..]).assign(sums);
+        });
+
+    result
 }
