3d49839db1bec5a11c2bd7ddd0cbdd310eee950a
xd009642 <danielmckenna93@gmail.com>
2019-02-14 21:57:56 +0000

    Attempted to fix conv some more
    Simplified trait bounds and made it so kernel and image didn't need to take the same time hoping that made things line up nicer..

--- examples/basic.rs
+++ examples/basic.rs
@@ -1,7 +1,7 @@
 
 use ndarray_vision::core::*;
 
-use ndarray::arr1;
+use ndarray::{arr1, arr3, Array3};
 
 fn main() {
     let format = PixelFormat::RGB;
@@ -15,7 +15,11 @@ fn main() {
 
     // Set top left pixel to RGB 1.0, 0.0, 0.0
     image.pixel_mut(0,0).assign(&arr1(&[1.0, 0.0, 0.0]));
-    
+
+    let k: Array3::<f64> = arr3(&[[[0.,0.,0.],[4.,4.,4.],[0.,0.,0.]],
+                        [[0.,0.,0.],[10.,10.,10.],[0.,0.,0.]],
+                        [[0.,0.,0.],[4.,4.,4.],[0.,0.,0.]]]);
+    image.conv(k.view());
     // Print pixel and image data to show change
     println!("{:?}", image.pixel(0, 0));
     println!("{:?}", image.data);
--- src/core/image.rs
+++ src/core/image.rs
@@ -14,7 +14,7 @@ pub struct Image<T> {
     format: PixelFormat,
 }
 
-impl<T> Image<T> where T: One, T: Zero, T: Clone, T: Default {
+impl<T> Image<T> where T: One + Zero + Clone + Default {
     pub fn new(rows: usize, columns: usize, format: PixelFormat) -> Self {
         Image {
             data: Array3::<T>::zeros((rows, columns, format.channels())),
@@ -30,30 +30,30 @@ impl<T> Image<T> where T: One, T: Zero, T: Clone, T: Default {
         self.data.slice_mut(s![row, col, ..])
     }
 
-    pub fn conv(&self, kernel: ArrayView3<T>) -> Image<T> {
+    pub fn conv<U>(&self, kernel: ArrayView3<U>) -> Image<T> {
         Image {
             data: conv(self.data.view(), kernel),
             format: self.format
         }
     }
 
-    pub fn conv_inplace(&mut self, kernel: ArrayView3<T>) {
+    pub fn conv_inplace<U>(&mut self, kernel: ArrayView3<U>) {
         self.data = conv(self.data.view(), kernel);
     }
 }
 
 
-pub fn conv<T>(image: ArrayView3<T>, kernel: ArrayView3<T>) -> Array3<T> where T: Default, T: Clone {
-    let mut result = Array3::<T>::default(image.dim());
+pub fn conv<T, U>(image: ArrayView3<T>, kernel: ArrayView3<U>) -> Array3<T> where T: Clone + Zero {
+    let mut result = Array3::<T>::zeros(image.dim());
     let k_s = kernel.shape();
     let row_offset = k_s[0]/2;
     let col_offset = k_s[1]/2;
 
     Zip::indexed(image.windows(kernel.dim()))
         .apply(|(i, j, _), window| {
-            let mult = window * kernel;
+            let mult =  window * kernel;
             let sums = mult.sum_axis(Axis(2));
-            result.slice_mut(s![i+row_offset, j+col_offset, ..]).assign(sums);
+            result.slice_mut(s![i+row_offset, j+col_offset, ..]).assign(&sums);
         });
 
     result
