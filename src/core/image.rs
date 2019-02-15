4aa7dfb02f2f48183fe5830ab87755f1e1faaec8
xd009642 <danielmckenna93@gmail.com>
2019-02-15 18:43:36 +0000

    Added some doc comments

--- src/core/image.rs
+++ src/core/image.rs
@@ -18,6 +18,8 @@ impl<T> Image<T>
 where
     T: One + Zero + Clone + Mul + Add + Sub,
 {
+    //! Construct a new image filled with zeros using the given dimensions and
+    //! pixel format
     pub fn new(rows: usize, columns: usize, format: PixelFormat) -> Self {
         Image {
             data: Array3::<T>::zeros((rows, columns, format.channels())),
@@ -25,14 +27,17 @@ where
         }
     }
 
+    /// Get a view of all colour channels at a pixels location
     pub fn pixel(&self, row: usize, col: usize) -> ArrayView<T, Ix1> {
         self.data.slice(s![row, col, ..])
     }
 
+    /// Get a mutable view of a pixels colour channels given a location
     pub fn pixel_mut(&mut self, row: usize, col: usize) -> ArrayViewMut<T, Ix1> {
         self.data.slice_mut(s![row, col, ..])
     }
 
+    /// Return a image formed when you convolve the image with a kernel
     pub fn conv(&self, kernel: ArrayView3<T>) -> Image<T> {
         Image {
             data: conv(self.data.view(), kernel),
@@ -40,11 +45,14 @@ where
         }
     }
 
+    /// Apply a convolution to the image
     pub fn conv_inplace(&mut self, kernel: ArrayView3<T>) {
         self.data = conv(self.data.view(), kernel);
     }
 }
 
+/// Implements a simple image convolution given a image and kernel
+/// TODO Add an option to change kernel centre
 pub fn conv<T>(image: ArrayView3<T>, kernel: ArrayView3<T>) -> Array3<T>
 where
     T: Clone + Zero + Mul<T, Output = T>,
