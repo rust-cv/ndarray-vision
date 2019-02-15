f12445bfdad899b2fec180ec6e1a1259ece0b54d
xd009642 <danielmckenna93@gmail.com>
2019-02-15 16:27:40 +0000

    Fixed convolution removed example printouts.
    T had to implement core::ops::Mul in order to use the multiplication operator

--- examples/basic.rs
+++ examples/basic.rs
@@ -7,20 +7,17 @@ fn main() {
     let format = PixelFormat::RGB;
     
     // Create an empty image
-    let mut image = Image::<f64>::new(5, 10, format);
+    let mut image = Image::<f64>::new(6, 6, format);
     
-    // Pixels all assigned to zero initially
-    println!("{:?}", image.pixel(0, 0));
-
-
     // Set top left pixel to RGB 1.0, 0.0, 0.0
     image.pixel_mut(0,0).assign(&arr1(&[1.0, 0.0, 0.0]));
+    
+    println!("{:?}", image.data);
 
-    let k: Array3::<f64> = arr3(&[[[0.,0.,0.],[4.,4.,4.],[0.,0.,0.]],
-                        [[0.,0.,0.],[10.,10.,10.],[0.,0.,0.]],
-                        [[0.,0.,0.],[4.,4.,4.],[0.,0.,0.]]]);
-    image.conv(k.view());
+    let mean = 1.0f64/9.0f64;
+    let kernel = Array3::from_elem((3,3,3), mean);
+    image.conv_inplace(kernel.view());
+    
     // Print pixel and image data to show change
-    println!("{:?}", image.pixel(0, 0));
     println!("{:?}", image.data);
 }
--- src/core/image.rs
+++ src/core/image.rs
@@ -1,20 +1,21 @@
 use crate::core::formats::PixelFormat;
 use ndarray::{ArrayView, ArrayView3, ArrayViewMut, Array3, Axis, Ix1, s, Zip};
 use num_traits::{Zero, One};
+use core::ops::{Mul, Add, Sub};
 
 
+/// Basic structure containing an image. 
 pub struct Image<T> {
     /// Images are always going to be 3D to handle rows, columns and colour 
     /// channels
     ///
-    /// 
     /// This should allow for max compatibility with maths ops in ndarray
     pub data: Array3<T>,
     /// Pixel format stored internally
     format: PixelFormat,
 }
 
-impl<T> Image<T> where T: One + Zero + Clone + Default {
+impl<T> Image<T> where T: One + Zero + Clone + Mul + Add + Sub {
     pub fn new(rows: usize, columns: usize, format: PixelFormat) -> Self {
         Image {
             data: Array3::<T>::zeros((rows, columns, format.channels())),
@@ -30,20 +31,21 @@ impl<T> Image<T> where T: One + Zero + Clone + Default {
         self.data.slice_mut(s![row, col, ..])
     }
 
-    pub fn conv<U>(&self, kernel: ArrayView3<U>) -> Image<T> {
+    pub fn conv(&self, kernel: ArrayView3<T>) -> Image<T> {
         Image {
             data: conv(self.data.view(), kernel),
             format: self.format
         }
     }
 
-    pub fn conv_inplace<U>(&mut self, kernel: ArrayView3<U>) {
+    pub fn conv_inplace(&mut self, kernel: ArrayView3<T>) {
         self.data = conv(self.data.view(), kernel);
     }
 }
 
 
-pub fn conv<T, U>(image: ArrayView3<T>, kernel: ArrayView3<U>) -> Array3<T> where T: Clone + Zero {
+pub fn conv<T>(image: ArrayView3<T>, kernel: ArrayView3<T>) -> Array3<T> 
+where T: Clone + Zero + Mul<T, Output=T> {
     let mut result = Array3::<T>::zeros(image.dim());
     let k_s = kernel.shape();
     let row_offset = k_s[0]/2;
@@ -51,8 +53,8 @@ pub fn conv<T, U>(image: ArrayView3<T>, kernel: ArrayView3<U>) -> Array3<T> wher
 
     Zip::indexed(image.windows(kernel.dim()))
         .apply(|(i, j, _), window| {
-            let mult =  window * kernel;
-            let sums = mult.sum_axis(Axis(2));
+            let mult =  &window * &kernel;
+            let sums = mult.sum_axis(Axis(0)).sum_axis(Axis(0));
             result.slice_mut(s![i+row_offset, j+col_offset, ..]).assign(&sums);
         });
 
