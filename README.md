37a687e75ed39b7c632be234710a45eb303e639b
xd009642 <danielmckenna93@gmail.com>
2019-02-21 18:56:58 +0000

    Documentation updates
    * Updated Cargo.toml with some stuff I'll need pre-publish
    * Wrote some root module docs for docs.rs pre-publish
    * Updated the README with some indication of current progress

--- README.md
+++ README.md
@@ -1,4 +1,11 @@
 # ndarray-vision
 
 This project is a computer vision library built on top of ndarray. This project
-is a work in progress and doesn't yet work. 
+is a work in progress. It is possible to load an image apply filters and save
+it back, however it is currently largely non-functional.
+
+# Features
+
+* RGB and HSV conversions
+* Image convolutions and some filters (box linear, gaussian)
+* Encoding and decoding PPM (binary or plaintext)
--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,33 @@
-/// ndarray-vision - A computer vision and image analysis crate built on ndarray
+//! This crate is a computer vision and image analysis crate built on `ndarray`.
+//!
+//! By using `ndarray`, this project aims to make full use of other crates in
+//! the ecosystem like `ndarray_stats`. This should also allow users to easily
+//! integrate other `ndarray` crates with `ndarray-vision` and avoid this crate
+//! becoming a monolith by fulfilling every potential usecase in a rather large
+//! field. Instead the main focus of this crate will be as follows:
+//!
+//! * An `Image` type which makes use of Rust to ensure proper and safe use
+//! * Conversions between different colour models
+//! * Encoding and decoding images
+//! * Image processing intrinsics like convolution and a selection of common filters
+//! * Common image enhancement algorithms
+//! * Geometric image transformations
+//! * Intrinsics required for feature detection and matching
+//! * Camera Calibration
+//! * Frequency domain image processing 
+//!
+//! This may seem like a lot but is still a lot less than OpenCV offers. Also,
+//! where possible algorithms will be used from other crates in the ecosystem
+//! when those operations aren't Computer Vision specific. For example, 
+//! `ndarray-stats` has histogram calculation.
+//!
+//! This crate is a work in progress and as such most of these features aren't
+//! yet present and those that are may not be stable. Although, there will be
+//! some effort to ensure things don't break.
+
+/// The core of `ndarray-vision` contains the `Image` type and colour models
 pub mod core;
+/// Image formats - encoding and decoding images from bytes for saving and loading
 pub mod format;
+/// Image processing intrinsics and common filters/algorithms.
 pub mod processing;
