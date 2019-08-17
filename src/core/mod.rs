a6beac2ff29c697057a91585567a830fbe9d99e7
xd009642 <danielmckenna93@gmail.com>
2019-08-17 16:23:23 +0100

    Add Padding implementation
    Added no padding and constant padding

--- src/core/mod.rs
+++ src/core/mod.rs
@@ -1,12 +1,14 @@
-
-/// This module deals with different colour models and conversions between 
+/// This module deals with different colour models and conversions between
 /// colour models.
 pub mod colour_models;
 /// Core image type and simple operations on it
 pub mod image;
+/// Image padding operations to increase the image size
+pub mod padding;
 /// Essential traits for the functionality of `ndarray-vision`
 pub mod traits;
 
 pub use colour_models::*;
 pub use image::*;
+pub use padding::*;
 pub use traits::*;
