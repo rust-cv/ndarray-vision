ca0fd502f4effa3c5ae6d16c740749d9c9b04dfe
xd009642 <danielmckenna93@gmail.com>
2021-12-15 13:43:00 +0000

    Remove lena (#49)

--- examples/transforms.rs
+++ examples/transforms.rs
@@ -9,17 +9,19 @@ use std::fs::File;
 use std::io::BufWriter;
 use std::path::{Path, PathBuf};
 
-fn get_lena() -> Option<Image<u8, RGB>> {
+fn get_cameraman() -> Option<Image<u8, RGB>> {
     if let Ok(mut root) = current_exe() {
         root.pop();
         root.pop();
         root.pop();
         root.pop();
-        let mut lena = PathBuf::from(&root);
-        lena.push("images/lena.ppm");
+        let mut cameraman = PathBuf::from(&root);
+        cameraman.push("images/cameraman.ppm");
 
         let decoder = PpmDecoder::default();
-        let image: Image<u8, _> = decoder.decode_file(lena).expect("Couldn't open Lena.ppm");
+        let image: Image<u8, _> = decoder
+            .decode_file(cameraman)
+            .expect("Couldn't open cameraman.ppm");
         Some(image)
     } else {
         None
@@ -27,19 +29,19 @@ fn get_lena() -> Option<Image<u8, RGB>> {
 }
 
 fn main() {
-    let lena = get_lena().expect("Couldn't load lena");
+    let cameraman = get_cameraman().expect("Couldn't load cameraman");
 
     // Create transformation matrix
-    let x = 0.5 * (lena.cols() as f64) - 0.5;
-    let y = 0.5 * (lena.rows() as f64) - 0.5;
+    let x = 0.5 * (cameraman.cols() as f64) - 0.5;
+    let y = 0.5 * (cameraman.rows() as f64) - 0.5;
     let trans = rotate_around_centre(FRAC_PI_4, (x, y)).dot(&scale(0.7, 0.7));
 
-    let transformed = lena
+    let transformed = cameraman
         .transform(trans.view(), None)
         .expect("Transform failed");
 
     // save
-    let path = Path::new("transformed_lena.png");
+    let path = Path::new("transformed_cameraman.png");
     let file = File::create(path).expect("Couldn't create output file");
     let ref mut w = BufWriter::new(file);
 
