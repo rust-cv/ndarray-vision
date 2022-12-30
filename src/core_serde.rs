d989cad2080d4ed37f15e0038d8caaa61d52f4fe
Christopher Field <chris.field@theiascientific.com>
2022-12-30 11:16:05 -0500

    Add unit test and minimum working implementation

--- src/core_serde.rs
+++ src/core_serde.rs
@@ -6,16 +6,29 @@ use serde::{Serialize, Serializer};
 impl<A, T, C> Serialize for ImageBase<T, C>
 where
     A: Serialize,
-    T: Data<Elem = A> + Serialize,
+    T: Data<Elem = A>,
     C: ColourModel,
 {
     fn serialize<S>(&self, serializer: S) -> Result<S::Ok, S::Error>
     where
         S: Serializer,
     {
-        let mut state = serializer.serialize_struct("Image", 2)?;
+        let mut state = serializer.serialize_struct("ImageBase", 2)?;
         state.serialize_field("data", &self.data)?;
-        state.serialize_field("model", &self.model)?;
+        state.serialize_field("model", C::NAME)?;
         state.end()
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use crate::core::{Image, RGB};
+
+    #[test]
+    fn serialize_image_base() {
+        const EXPECTED: &str = r#"{"data":{"v":1,"dim":[2,3,3],"data":[0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0]},"model":"RGB"}"#;
+        let i = Image::<u8, RGB>::new(2, 3);
+        let actual = serde_json::to_string(&i).expect("Serialized RGB image");
+        assert_eq!(actual, EXPECTED);
     }
 }
